68e1b31ef565d8d1bb6ab3c2f721e3f58ecb8386
liulianwanfan <3557452792@qq.com>
2020-09-27 23:59:02 +0800

    2.0

--- controllers/goods.go
+++ controllers/goods.go
@@ -10,44 +10,58 @@ import (
 )
 
 type GoodController struct {
-	 beego.Controller
+	beego.Controller
 }
 
-
 //该方法用于处理post类请求
-func  (c *GoodController)Post() {
-	fmt.Println(c == nil)
-	fmt.Println(c.Ctx == nil)
-	fmt.Println(c.Ctx.Request == nil)
-	//接收前端传递的数据
-	bodyBytes,err:=ioutil.ReadAll(c.Ctx.Request.Body)
-	if err != nil {
-		fmt.Println(err.Error(),"我错了")
-		c.Ctx.WriteString("数据库接收错误,请认真检查")
-		return
-	}
-	var info models.Info//调用info结构体
-	err=json.Unmarshal(bodyBytes,&info)
-	if err != nil {
-		fmt.Println(err.Error(),"我错了")
-		c.Ctx.WriteString("数据解析错误")
-		return
-	}
+func (c *GoodController) Post() {
 
-	//3.将解析到用户的数据,保存到数据库
-	id,err:=db_mysql.InsertInfo(info)
-	if err != nil {
-		fmt.Println(err.Error(),"我错了")
-		c.Ctx.WriteString("数据解析错误")
+	//1、获取请求数据
+	userName := c.Ctx.Input.Query("user")
+	password := c.Ctx.Input.Query("psd")
+	//2、使用固定数据进行数据校验
+	//admin 123456
+	if userName != "admin" && password != "123546" {
+		//代表错误处理
+		c.Ctx.ResponseWriter.Write([]byte("对不起,数据校验错误"))
 		return
-	}
-	fmt.Println(id)
 
-	result:=models.ResponseResult{
-		Code: 0,
-		Message: "保存成功",
-		Date: nil,
+		fmt.Println(c == nil)
+		fmt.Println(c.Ctx == nil)
+		fmt.Println(c.Ctx.Request == nil)
+		//接收前端传递的数据
+		bodyBytes, err := ioutil.ReadAll(c.Ctx.Request.Body)
+		if err != nil {
+			fmt.Println(err.Error(), "我错了")
+			c.Ctx.WriteString("数据库接收错误,请认真检查")
+			return
+		}
+		var info models.Info //调用info结构体
+		err = json.Unmarshal(bodyBytes, &info)
+		if err != nil {
+			fmt.Println(err.Error(), "我错了")
+			c.Ctx.WriteString("数据解析错误")
+			return
+		}
+
+		//3.将解析到用户的数据,保存到数据库
+		id, err := db_mysql.InsertInfo(info)
+		if err != nil {
+			fmt.Println(err.Error(), "我错了")
+			c.Ctx.WriteString("数据解析错误")
+			return
+		}
+		fmt.Println(id)
+
+		result := models.ResponseResult{
+			Code:    0,
+			Message: "保存成功",
+			Date:    nil,
+		}
+		c.Data["json"] = &result
+		c.ServeJSON()
+		c.Data["Website"] = "beego.me"
+		c.Data["Email"] = "astaxie@gmail.com"
+		c.TplName = "views/good.html"
 	}
-	c.Data["json"]=&result
-	c.ServeJSON()
 }
--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	_ "NewBeegoProject/routers"
 	"github.com/astaxie/beego"
 )
-//
+
 //func init() {
 //	beego.BConfig.CopyRequestBody = true
 //}
-
+//
 
 func main() {
 	beego.Run()
+
 }
 
